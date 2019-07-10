61166c481f14a138036d8ae9c8f247606be3a133
GrevillotL <loic.grevillot@medaustron.at>
2019-07-10 20:17:33 +0200

    updated feature for the efficiency scoring in the doseActor

--- source/digits_hits/include/GateDoseActorMessenger.hh
+++ source/digits_hits/include/GateDoseActorMessenger.hh
@@ -19,6 +19,7 @@
 #include "G4UIcmdWithABool.hh"
 #include "G4UIcmdWithAString.hh"
 #include "GateImageActorMessenger.hh"
+#include "GateMiscFunctions.hh"
 
 class GateDoseActor;
 class GateDoseActorMessenger : public GateImageActorMessenger
@@ -42,7 +43,10 @@ protected:
   G4UIcmdWithABool * pEnableDoseUncertaintyCmd;
   G4UIcmdWithABool * pEnableDoseNormToMaxCmd;
   G4UIcmdWithABool * pEnableDoseNormToIntegralCmd;
+    //Efficiency option
   G4UIcmdWithAString * pSetDoseEfficiencyCmd;
+    //Efficiency option by Z (by ion atomic number)
+  G4UIcmdWithAString * pSetDoseEfficiencyByZCmd;
   //DoseToWater
   G4UIcmdWithABool * pEnableDoseToWaterCmd;
   G4UIcmdWithABool * pEnableDoseToWaterSquaredCmd;
